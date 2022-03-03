494d605b1f640a9aa797ac33f82b9ac957f96548
Dishant Nagpal <dishantnagpal10@gmail.com>
2022-03-03 13:41:29 +0530

    Start commit

---
+++ MyMessenger/Controller/VideoPlayerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VideoPlayerViewController.swift
+//  MyMessenger
+//
+//  Created by Dishant Nagpal on 03/03/22.
+//
+
+import UIKit
+
+class VideoPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
